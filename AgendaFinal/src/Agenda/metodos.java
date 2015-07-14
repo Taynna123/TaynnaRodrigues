5154bf4bd0243121beb976567a168471ae219245
Taynna <taynnamarttins@gmail.com>
2015-07-14 02:05:53 -0300

    Agenda Teste

---
+++ AgendaFinal/src/Agenda/metodos.java
@@ -0,0 +1,141 @@
+package Agenda;
+
+public class metodos {
+    
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String email;
+    private String descricao;
+    private String data;
+    private String hora;
+    private String local;
+    private String usuario;
+    private int idade;
+    private int DatNasc;
+    private String senha;
+    private String login;
+    private String descricaao;
+    private Boolean pendente;
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }  
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getDatNasc() {
+        return DatNasc;
+    }
+
+    public void setDatNasc(int DatNasc) {
+        this.DatNasc = DatNasc;
+    }
+
+    public String getDescricaao() {
+        return descricaao;
+    }
+
+    public void setDescricaao(String descricaao) {
+        this.descricaao = descricaao;
+    }
+
+    public Boolean getPendente() {
+        return pendente;
+    }
+
+    public void setPendente(Boolean pendente) {
+        this.pendente = pendente;
+    }
+    
+}
